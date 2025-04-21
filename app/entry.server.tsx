76f081b3eb928c8a8d37501e9d92b01ce8555c45
Brian <bwclovis.web@gmail.com>
2025-04-21 08:09:56 -0400

    setup server/nonce

---
+++ app/entry.server.tsx
@@ -0,0 +1,66 @@
+import { PassThrough } from "node:stream";
+
+import type { AppLoadContext, EntryContext } from "react-router";
+import { createReadableStreamFromReadable } from "@react-router/node";
+import { ServerRouter } from "react-router";
+import { isbot } from "isbot";
+import type { RenderToPipeableStreamOptions } from "react-dom/server";
+import { renderToPipeableStream } from "react-dom/server";
+import { NonceProvider } from '~/hooks/use-nonce'
+export const streamTimeout = 5_000;
+
+export default function handleRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  routerContext: EntryContext,
+  loadContext: AppLoadContext
+  // If you have middleware enabled:
+  // loadContext: unstable_RouterContextProvider
+) {
+  return new Promise((resolve, reject) => {
+    const nonce = String(loadContext.cspNonce) ?? undefined;
+    let shellRendered = false;
+    let userAgent = request.headers.get("user-agent");
+    let readyOption: keyof RenderToPipeableStreamOptions =
+      (userAgent && isbot(userAgent)) || routerContext.isSpaMode
+        ? "onAllReady"
+        : "onShellReady";
+
+    const { pipe, abort } = renderToPipeableStream(
+      <NonceProvider value={nonce}>
+        <ServerRouter context={routerContext} url={request.url} />
+      </NonceProvider>,
+      {
+        [readyOption]() {
+          shellRendered = true;
+          const body = new PassThrough();
+          const stream = createReadableStreamFromReadable(body);
+
+          responseHeaders.set("Content-Type", "text/html");
+
+          resolve(
+            new Response(stream, {
+              headers: responseHeaders,
+              status: responseStatusCode,
+            })
+          );
+
+          pipe(body);
+        },
+        onShellError(error: unknown) {
+          reject(error);
+        },
+        onError(error: unknown) {
+          responseStatusCode = 500;
+
+          if (shellRendered) {
+            console.error(error);
+          }
+        },
+      }
+    );
+
+    setTimeout(abort, streamTimeout + 1000);
+  });
+}
---
+++ server.mjs
@@ -0,0 +1,148 @@
+import crypto from 'crypto'
+import process from 'node:process'
+
+import prom from '@isaacs/express-prometheus-middleware'
+import { createRequestHandler } from '@react-router/express'
+import compression from 'compression'
+import express from 'express'
+import { rateLimit } from 'express-rate-limit'
+import helmet from 'helmet'
+import morgan from 'morgan'
+
+const METRICS_PORT = process.env.METRICS_PORT || 3030
+const PORT = process.env.APP_PORT || 2112
+const NODE_ENV = process.env.NODE_ENV ?? 'development'
+const MAX_LIMIT_MULTIPLE = NODE_ENV !== 'production' ? 10_000 : 1
+
+const viteDevServer =
+  process.env.NODE_ENV === "production"
+    ? undefined
+    : await import("vite").then(vite => vite.createServer({
+          server: { middlewareMode: true }
+        }))
+
+const defaultRateLimit = {
+  legacyHeaders: false,
+  max: 1000 * MAX_LIMIT_MULTIPLE,
+  standardHeaders: true,
+  windowMs: 60 * 1000
+}
+
+const strongestRateLimit = rateLimit({
+  ...defaultRateLimit,
+  max: 10 * MAX_LIMIT_MULTIPLE,
+  windowMs: 60 * 1000
+})
+
+const strongRateLimit = rateLimit({
+  ...defaultRateLimit,
+  max: 100 * MAX_LIMIT_MULTIPLE,
+  windowMs: 60 * 1000
+})
+const generalRateLimit = rateLimit(defaultRateLimit)
+
+const app = express()
+const metricsApp = express()
+
+if (viteDevServer) {
+  app.use("/assets", express.static("public/assets")) 
+  app.use(viteDevServer.middlewares)
+} else {
+  app.use(
+    "/assets",
+    express.static("build/client/assets", {
+      immutable: true,
+      maxAge: "1y"
+    })
+  )
+}
+app.use(express.static("build/client", { maxAge: "1h" }))
+app.disable('x-powered-by')
+app.use(compression())
+app.use(morgan('tiny'))
+
+// Prometheus
+app.use(prom({
+  collectDefaultMetrics: true,
+  metricsApp,
+  metricsPath: "/metrics"
+}))
+
+app.use((_, res, next) => {
+  res.locals.cspNonce = crypto.randomBytes(16).toString('hex')
+  next()
+})
+
+app.use(helmet({
+  crossOriginEmbedderPolicy: false,
+  contentSecurityPolicy: {
+      directives: {
+        'connect-src': [ NODE_ENV === 'development' ? 'ws:' : null, "'self'" ].filter(Boolean),
+        'font-src': [ "'self'" ],
+        'frame-src': [ "'self'" ],
+        'img-src': [ "'self'", 'data:' ],
+        'script-src': [
+          "'strict-dynamic'",
+          "'self'",
+          (_, res) => `'nonce-${res.locals.cspNonce}'`
+        ],
+        'script-src-attr': [ (_, res) => `'nonce-${res.locals.cspNonce}'` ],
+        'upgrade-insecure-requests': null
+      },
+      // ❗Important: Remove `reportOnly` to enforce CSP. (Development only).
+      referrerPolicy: { policy: 'same-origin' },
+      reportOnly: true
+    }
+  }))
+
+app.use((req, res, next) => {
+  if (req.path.endsWith('/') && req.path.length > 1) {
+    const query = req.url.slice(req.path.length)
+    const safePath = req.path.slice(0, -1).replace(/\/+/g, '/')
+    res.redirect(301, safePath + query)
+  } else {
+    next()
+  }
+})
+
+// eslint-disable-next-line complexity
+app.use((req, res, next) => {
+  const STRONG_PATHS = [ '/auth/login' ]
+  if (req.method !== 'GET' && req.method !== 'HEAD') {
+    if (STRONG_PATHS.some(path => req.path.includes(path))) {
+      return strongestRateLimit(req, res, next)
+    }
+    return strongRateLimit(req, res, next)
+  }
+  return generalRateLimit(req, res, next)
+})
+
+const build = viteDevServer
+  ? () => viteDevServer.ssrLoadModule("virtual:react-router/server-build")
+  : await import("./build/server/index.js")
+
+app.all(
+  "*",
+  createRequestHandler({
+    build,
+    getLoadContext: (_, res) => ({
+      cspNonce: res.locals.cspNonce
+    })
+  })
+)
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  if (res.headersSent) return next(err)
+
+  res.status(500).send(
+    NODE_ENV === 'development'
+      ? `<pre>${err.stack}</pre>`
+      : "Internal Server Error"
+  )
+})
+
+
+
+app.listen(PORT, () => console.log(`🤘 server running: http://localhost:${PORT}`))
+metricsApp.listen(METRICS_PORT, () => console.log(`✅ metrics ready: http://localhost:${METRICS_PORT}/metrics`))
