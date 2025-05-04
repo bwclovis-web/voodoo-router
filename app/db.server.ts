777f7ae857884862017f1e1a6869f87c57dffdc5
Brian <bwclovis.web@gmail.com>
2025-05-04 11:02:31 -0400

    updates

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,8 +1,8 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from '@prisma-app/client'
 
-import { singleton } from "./utils/server/utility.server"
+import { singleton } from './utils/server/utility.server'
 
-const prisma = singleton("prisma", () => new PrismaClient())
+const prisma = singleton('prisma', () => new PrismaClient())
 prisma.$connect()
 
 export { prisma }
